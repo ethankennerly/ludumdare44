ce562b7324831e474de1907fec36d4dd791a140a
Ethan Kennerly <ethankennerly@gmail.com>
2019-04-27 12:10:19 -0700

    Map Controller: Extracts from Word Link.

---
+++ LudumDare44/Assets/Scripts/MapController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapController : BaseController {
+    public ScrollRect scrollRect;
+    public Transform levelContent;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        CUtils.ShowChildInScrollView(scrollRect, Prefs.UnlockedLevel, levelContent);
+    }
+}
