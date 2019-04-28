7fd96ae4ffe59037d8c96bbd643dac6b70af9e3c
Ethan Kennerly <ethankennerly@gmail.com>
2019-04-27 18:25:09 -0700

    Char Level Instantiator: Reveals letter on level.

--- LudumDare44/Assets/Scripts/CharLevelInstantiator.cs
+++ LudumDare44/Assets/Scripts/CharLevelInstantiator.cs
@@ -12,12 +12,18 @@ namespace FineGameDesign.Text
         [SerializeField]
         private GameObject m_NextLevelPrefab;
 
+        [SerializeField]
+        private int m_OverrideCurrentIndex = -1;
+
         /// <remarks>
         /// Spawn prefabs at positions.
         /// TODO: For pre/cur levels, reveal letters.
         /// </remarks>
         public void Instantiate(int currentIndex, Transform parent, char[] characters, Vector3[] localPositions)
         {
+            if (m_OverrideCurrentIndex >= 0)
+                currentIndex = m_OverrideCurrentIndex;
+
             int numLevels = localPositions.Length;
             if (currentIndex >= numLevels)
                 currentIndex = numLevels - 1;
@@ -26,14 +32,19 @@ namespace FineGameDesign.Text
             {
                 GameObject previousClone = Instantiate(m_PreviousLevelPrefab, parent);
                 ((RectTransform)(previousClone.transform)).anchoredPosition = localPositions[previousIndex];
+                LevelButton previousButton = previousClone.GetComponent<LevelButton>();
+                previousButton.LevelIndex = previousIndex;
+                previousButton.LevelNumberText.text = characters[previousIndex].ToString();
             }
 
             GameObject currentClone = Instantiate(m_CurrentLevelPrefab, parent);
             ((RectTransform)(currentClone.transform)).anchoredPosition = localPositions[currentIndex];
+            LevelButton currentButton = currentClone.GetComponent<LevelButton>();
+            currentButton.LevelIndex = currentIndex;
+            currentButton.LevelNumberText.text = characters[currentIndex].ToString();
 
             for (int nextIndex = currentIndex + 1; nextIndex < numLevels; ++nextIndex)
             {
-                Vector3 nextPosition = parent.TransformPoint(localPositions[nextIndex]);
                 GameObject nextClone = Instantiate(m_NextLevelPrefab, parent);
                 ((RectTransform)(nextClone.transform)).anchoredPosition = localPositions[nextIndex];
             }
---
+++ LudumDare44/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class LevelButton : MonoBehaviour
+{
+    public Sprite passed;
+    public Image image;
+
+    [SerializeField]
+    private TMP_Text m_LevelNumberText;
+    public TMP_Text LevelNumberText
+    {
+        get { return m_LevelNumberText; }
+    }
+
+    public int LevelIndex { get; set; }
+
+    public void OnButtonClick()
+    {
+        Prefs.CurrentLevel = LevelIndex;
+        CUtils.LoadScene(2, true);
+    }
+}
